803b5c9a7e260d4964e77389967326fa9debe0f7
kanjitta <jittakan2539@gmail.com>
2024-05-14 14:07:03 +0700

    initial commit

---
+++ script1_1.js
@@ -0,0 +1,31 @@
+// Exercise 1.1
+function add(x, y) {
+    return x + y;
+
+}
+
+function subtract(x, y) {
+    return x - y;
+
+}
+
+function multiply(x, y) {
+    return x * y;
+
+}
+
+function divide(x, y) {
+    return x / y;
+
+}
+
+console.log(add(1, 2));
+
+console.log(subtract(1, 2));
+
+
+console.log(multiply(1, 2));
+
+
+console.log(divide(1, 2));
+
---
+++ script1_2.js
@@ -0,0 +1,7 @@
+// Exercise 1.2
+
+function Inform(firstName, location, hobby) {
+    return `Hi, my name is ${firstName}. I live in ${location} and enjoy ${hobby}.`;
+}
+
+console.log(Inform('Kan', 'BKK', 'reading'));
